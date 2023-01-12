e04164e029edd0dabbc9d1d367e2877eb111b73d
Evelin Aasna <evelin.aasna@fu-berlin.de>
2023-01-12 15:53:09 +0100

    [TEST] API test _getDatabaseSegments

--- include/stellar/stellar_database_segment.hpp
+++ include/stellar/stellar_database_segment.hpp
@@ -1,7 +1,10 @@
 
 #pragma once
 
+#include <seqan/seq_io.h>
+
 #include <stellar/stellar_sequence_segment.hpp>
+#include <stellar/stellar_types.hpp>
 
 namespace stellar
 {
@@ -39,5 +42,40 @@ struct StellarDatabaseSegment : public StellarSequenceSegment<TAlphabet>
         return finderSegment;
     }
 };
+
+template <typename TAlphabet, typename TStorage>
+TStorage _getDatabaseSegments(StringSet<String<TAlphabet>> & databases, StellarOptions const & options, bool const reverse = false)
+{
+    TStorage databaseSegments{};
+    if (options.prefilteredSearch)
+    {
+        if (options.sequenceOfInterest >= length(databases))
+            throw std::runtime_error{"Sequence of interest out of range"};
+
+        if (length(databases[options.sequenceOfInterest]) < options.segmentEnd)
+            throw std::runtime_error{"Segment end out of range"};
+
+        if (options.segmentEnd <= options.segmentBegin)
+            throw std::runtime_error{"Incorrect segment definition"};
+
+        if (options.segmentEnd < options.minLength + options.segmentBegin)
+            throw std::runtime_error{"Segment shorter than minimum match length"};
+
+        if (reverse)
+            reverseComplement(databases[options.sequenceOfInterest]);
+        databaseSegments.emplace_back(databases[options.sequenceOfInterest], options.segmentBegin, options.segmentEnd);
+    }
+    else
+        for (auto & database : databases)
+        {
+            if (reverse)
+                reverseComplement(database);
+
+            if (length(database) >= options.minLength)
+                databaseSegments.emplace_back(database, 0u, length(database));
+        }
+
+    return databaseSegments;
+}
 
 } // namespace stellar
