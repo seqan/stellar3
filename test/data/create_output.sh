667e39cd89b4059a5fa7ae0ba4df53ac86ae80a1
Evelin Aasna <evelin.aasna@fu-berlin.de>
2023-01-13 16:30:09 +0100

    [TEST] how much does match have to overlap segment?

--- test/data/create_output.sh
+++ test/data/create_output.sh
@@ -12,7 +12,54 @@ rm full.gff
 rm full.stdout
 
 errRate=0.05
-${STELLAR} -e $errRate -l 50 -v --suppress-runtime-printing -o full.gff ref.fasta query_e${errRate}.fasta > full.stdout
-${STELLAR} -e $errRate -l 50 -v --sequenceOfInterest 0 --segmentBegin 0 --segmentEnd 400 --suppress-runtime-printing -o 0_0_400.gff ref.fasta query_e${errRate}.fasta > 0_0_400.stdout
-${STELLAR} -e $errRate -l 50 -v --sequenceOfInterest 0 --segmentBegin 200 --segmentEnd 600 --suppress-runtime-printing -o 0_200_600.gff ref.fasta query_e${errRate}.fasta > 0_200_600.stdout
-${STELLAR} -e $errRate -l 50 -v --sequenceOfInterest 0 --segmentBegin 400 --segmentEnd 700 --suppress-runtime-printing -o 0_400_700.gff ref.fasta query_e${errRate}.fasta > 0_400_700.stdout
+
+call_stellar () {
+   ${STELLAR} -e $errRate -l 50 -v -k 15 --sequenceOfInterest 0 --segmentBegin $1 --segmentEnd $2 --suppress-runtime-printing -o 0_$1_$2.gff ref.fasta query_e${errRate}.fasta > 0_$1_$2.stdout
+}
+
+${STELLAR} -e $errRate -l 50 -v -k 15 --suppress-runtime-printing -o full.gff ref.fasta query_e${errRate}.fasta > full.stdout
+
+############################################################
+# Extend left
+############################################################
+
+# Stellar	eps-matches	28	203
+# overlap match by 24 (extend left)
+call_stellar 179 300
+# overlap match by 25 (extend left)
+call_stellar 178 300
+
+# Stellar	eps-matches	126	302
+# overlap match by 31 (extend left)
+call_stellar 271 400
+# overlap match by 32 (extend left)
+call_stellar 270 400
+
+# Stellar	eps-matches	177	356
+# overlap match by 14 (extend left)
+call_stellar 342 450
+# overlap match by 15 (extend left)
+call_stellar 341 450
+
+
+############################################################
+# Extend right
+############################################################
+
+# Stellar	eps-matches	628	813
+# overlap match by 15 (extend right)
+call_stellar 500 643
+# overlap match by 16 (extend right)
+call_stellar 500 644
+
+# Stellar	eps-matches	708	761
+# overlap match by 13 (extend right)
+call_stellar 600 721
+# overlap match by 14 (extend right)
+call_stellar 600 722
+
+# Stellar	eps-matches	749	943
+# overlap match by 13 (extend right)
+call_stellar 600 762
+# overlap match by 14 (extend right)
+call_stellar 600 763
