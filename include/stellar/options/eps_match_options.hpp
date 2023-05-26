6278f2917fe5d6bef2618363eada70627dd14cc8
Evelin Aasna <evelin.aasna@fu-berlin.de>
2023-05-26 10:51:50 +0200

    Min length size_t

--- include/stellar/options/eps_match_options.hpp
+++ include/stellar/options/eps_match_options.hpp
@@ -10,7 +10,7 @@ struct EPSMatchOptions
 {
     stellar::utils::fraction epsilon{5, 100}; // maximal error rate
     double numEpsilon{0.05};
-    unsigned minLength{100}; // minimal length of an epsilon-match
+    size_t minLength{100}; // minimal length of an epsilon-match
 };
 
 } // namespace stellar
