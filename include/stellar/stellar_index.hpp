261e555158bcc9ad82f045fc91a72dfb3033b518
Evelin Aasna <evelin.aasna@fu-berlin.de>
2023-09-05 14:00:12 +0200

    Fix

--- include/stellar/stellar_index.hpp
+++ include/stellar/stellar_index.hpp
@@ -58,6 +58,10 @@ struct StellarIndex
         : StellarIndex{convertImplStringSet(queries), options}
     {}
 
+    StellarIndex(StringSet<TInfixSegment> const & queries, IndexOptions const & options)
+        : StellarIndex{convertSegmentStringSet(queries), options}
+    {}
+
     StellarIndex(std::span<TInfixSegment> const & queries, IndexOptions const & options)
         : StellarIndex{convertImplSpan(queries), options}
     {}
@@ -112,6 +116,12 @@ private:
         for (TSequence const & query: queries)
             seqan2::appendValue(dependentQueries, seqan2::infix(query, 0, seqan2::length(query)));
 
+        return dependentQueries;
+    }
+
+    static TQGramStringSet convertSegmentStringSet(StringSet<TInfixSegment> const & queries)
+    {
+        TQGramStringSet dependentQueries = queries;
         return dependentQueries;
     }
 
